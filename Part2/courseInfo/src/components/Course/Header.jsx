1d5daa51d4c97639bca26841ca376a2e89807f58
telzeez <Abdlazeezolasunkanmi@gmail.com>
2026-07-22 04:09:59 +0100

    completed Part2 excercises

--- Part2/courseInfo/src/components/Course/Header.jsx
+++
@@ -1,5 +0,0 @@
-export const Header = ({course}) => {
-    return <h2 id={course.id}>{
-        course.name
-    }</h2>
-}
--- Part2/courseInfo/src/components/Course/Total.jsx
+++
@@ -1,9 +0,0 @@
-export const Total = ({coursePart}) => {
-console.log(coursePart)
-    return <p>
-        <strong>
-         Total of  {coursePart.reduce((a,b) => a + b.exercises, 0)} exercises
-    </strong> 
-
-    </p>
-}
