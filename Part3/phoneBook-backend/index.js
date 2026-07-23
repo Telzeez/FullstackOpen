453281cc1bc3fdcc5400378a93ff463dcbc83889
telzeez <Abdlazeezolasunkanmi@gmail.com>
2026-07-23 06:49:41 +0100

    "added env variables"

--- Part3/phoneBook-backend/index.js
+++ Part3/phoneBook-backend/index.js
@@ -1,15 +1,58 @@
 import express, { json } from 'express' 
 import { data as initialData } from './data.js' 
-import cors from 'cors'
+import mongoose, { mongo } from 'mongoose'
+
 import morgan from 'morgan'
+import { stringify } from 'node:querystring'
+import Phone from './phone.js'
+import phone from './phone.js'
+
+
+
+
 
 
 const app = express() 
 app.use(json()) 
-app.use(cors())
+
 app.use(morgan('dev'))
+app.use(express.static('dist'))
 let data = initialData
-const port = 3000 
+const port = process.env.PORT || 3001 
+
+// if (process.argv.length < 2) {
+//     console.log("run the command with your password and contact to add");
+//     process.exit(1)
+// }
+// const password = process.argv[2]
+// // get mongoose url
+// const url =  `mongodb+srv://Phonebook:${password}@cluster0.ix2etki.mongodb.net/phonebook_db`
+// // enable strict query and create connection
+// mongoose.set('strictQuery', true)
+// mongoose.connect(url, {family: 4})
+// // create a schema blueprint for the note Object
+// const phoneSchema = new mongoose.Schema({
+//     name: String,
+//     number: String
+// })
+// phoneSchema.set('toJSON', {
+//     transform: (document, returnedObject) =>{
+//         returnedObject.id = returnedObject._id.toString()
+//         delete returnedObject._id
+//         delete returnedObject.__v
+//     }
+// })
+// const Phone = mongoose.model('Phone', phoneSchema)
+// // if (process.argv.length === 3) {
+// //     Phone.find({}).then(result =>{
+// //         result.forEach(phone => {
+// //             console.log(phone)
+// //         });
+// //         mongoose.connection.close()
+// //     })
+// // }
+
+
 
 
 const longFormatter = new Intl.DateTimeFormat('en-US', {
@@ -28,7 +71,9 @@ app.get("/", (req, res) => {
 }) 
 
 app.get("/api/persons", (req, res) => { 
-    res.status(200).json(data) 
+    Phone.find({}).then(result => {
+        res.json(result)
+    })
 }) 
 app.get('/info', (req, res) => {
     const dateRequested = new Date()
@@ -61,13 +106,18 @@ app.post('/api/persons', (req, res) => {
     if(thisUserExist){
         return res.status(409).send(`The user with the name ${thisUserExist.name} exist`)
     }
-    const newPerson = {
+    const newPerson = new Phone({
         id: newId,
         name: person.name,
         number: person.number
     
-    }
-    data = data.concat(newPerson)
+    })
+    phone.save().then(savedPhone => {
+        console.log(savedPhone)
+        res.json(savedPhone)
+
+    })
+ 
     res.status(201).json(newPerson)
 })
 
---
+++ Part3/phoneBook-backend/phone.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import 'dotenv/config'
+import  dns from 'node:dns'
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+mongoose.set('strictQuery', true)
+const url = process.env.MONGODB_URI
+
+
+mongoose.connect(url, {family:4}).then(result => {
+    console.log('connected to MongoDB')
+}).catch(error => {
+    console.log('Error conneting to mongoDB: ', error.message)
+})
+const phoneSchema = new mongoose.Schema({
+    name: String, 
+    number: String
+})
+phoneSchema.set('toJSON', {
+    transform: (document, retrunedObj) => {
+         delete returnedObject._id
+         delete returnedObject.__v
+    }
+})  
+
+export default mongoose.model('Phone', phoneSchema)
